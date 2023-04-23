217055ace0b36346b8e30b00815e6fd981bc66fc
Josimar Jauregui <josimar.jauregui.negocios@gmail.com>
2023-04-23 01:31:42 -0500

    prueba consulta

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -10,10 +10,10 @@ class WebController extends Controller
     public function index(Request $request){
 
         $search=$request->search;
-        if(!empty($request->search)){
+        if(!empty($search)){
             $recetas=Receta::with('image')
                         ->with('receta_tipo')
-                        ->where('estado','1')
+                        //->where('estado','1')
                         ->where('nombre','like','%'.$search.'%')
                         ->orWhere('descripcion','like','%'.$search.'%')
                         ->orWhere('indicaciones','like','%'.$search.'%')
@@ -23,7 +23,7 @@ class WebController extends Controller
             
         }else{
             $recetas=Receta::with('image')
-            ->where('estado','1')
+            //->where('estado','1')
             ->orderBy('receta_tipo_id','asc')
             ->orderBy('nombre', 'asc')
             ->paginate(12);
